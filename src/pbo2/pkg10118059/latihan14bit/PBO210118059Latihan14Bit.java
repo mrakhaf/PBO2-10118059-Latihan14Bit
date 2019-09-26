552266f3e1c9904f43447d535a66a7da9d63629d
mrakhaf <48128093+mrakhaf@users.noreply.github.com>
2019-09-26 18:00:32 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118059/latihan14bit/PBO210118059Latihan14Bit.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118059.latihan14bit;
+
+/**
+ *
+ * @author 
+ * Nama  : Muhammad Rakha Firdaus
+ * NIM   : 10118059
+ * Kelas : IF-2
+ */
+public class PBO210118059Latihan14Bit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 60;
+        int b = 13;
+        int c = 0;
+        c = a & b;
+        System.out.println("a & b = " + c);
+        
+        c = a | b;
+        System.out.println("a | b = " + c);
+        
+        c = a ^ b;
+        System.out.println("a ^ b = " + c);
+        
+        c = ~a;
+        System.out.println("~a = " + c);
+        
+        c = a << 2;
+        System.out.println("a << 2 = " + c);
+        
+        c = a >> 2;
+        System.out.println("a >> 2 = " + c);
+    }
+    
+}
